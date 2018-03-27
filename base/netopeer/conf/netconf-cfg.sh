11b8d89673d8b636cf55dc465d0b9102da1ef0d0
Valentin <vk18496@gmail.com>
2018-03-27 22:42:39 +0200

    Track things before break something and waste 1 week to recover the previous status...

---
+++ base/netopeer/conf/netconf-cfg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+# sleep $ONOS_SUBMIT_WAIT
+
+NETCONF_USER=${NETCONF_USER-"root"}
+NETCONF_PASSWORD=${NETCONF_PASSWORD-"root"}
+
+while ! curl -s --user $ONOS_USER:$ONOS_PASSWORD -X POST -H "Content-Type: application/json" http://${ONOS_IP}:8181/onos/v1/network/configuration | grep -q '{.*"code":400.*}'; do
+	echo "Waiting..."
+	sleep 0.5
+done
+
+sleep 3
+
+cfg=$(cat <<EOF
+{
+  "devices": {
+    "netconf:$(hostname --ip-address):830": {
+      "netconf": {
+        "ip": "$(hostname --ip-address)",
+        "port": 830,
+        "username": "$NETCONF_USER",
+        "password": "$NETCONF_PASSWORD"
+      },
+      "basic": {
+        "driver": "ovs-netconf"
+      }
+    }
+  }
+}
+EOF
+)
+
+echo $cfg | curl -X POST -H "content-type:application/json" http://${ONOS_IP}:8181/onos/v1/network/configuration -d @- --user $ONOS_USER:$ONOS_PASSWORD
