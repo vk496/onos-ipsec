7dd0914886494a60e60ece5c40f4942bd37bafca
Valentin <vk18496@gmail.com>
2018-04-14 20:28:04 +0200

    minor fixes

--- up.sh
+++ up.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-docker-compose up -d --build
+if ! [[ $1 =~ ^[0-9]+$ ]] && [[ $1 -gt 0 ]]; then
+	docker-compose up -d --build
+else
+	echo Deploy with $1 instances
+	sleep 0.3
+	docker-compose up -d --build --scale netopeer=$1
+fi
